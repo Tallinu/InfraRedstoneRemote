174c4500eb264aa98c2e2a297ba6c3b69a8b52e1
Yancarlo Ramsey <yancarlo@gmail.com>
2012-05-25 20:42:37 -0700

    Fleshing out functionality

--- src/client/com/kaijin/InfraRedstoneRemote/BlockIRReceiver.java
+++ src/client/com/kaijin/InfraRedstoneRemote/BlockIRReceiver.java
@@ -4,10 +4,88 @@ import net.minecraft.src.*;
 
 public class BlockIRReceiver extends Block
 {
-
     public BlockIRReceiver(int i, int j)
     {
         super(i, j, Material.ground);
     }
 
+    @Override
+    public boolean hasTileEntity(int metadata)
+    {
+        return true;
+    }
+
+    @Override
+    public TileEntity getTileEntity(int metadata)
+    {
+        return new TileReceiver();
+    }
+
+    @Override
+    public boolean canProvidePower()
+    {
+        return true;
+    }
+
+    /**
+     * Is this block indirectly powering the block on the specified side
+     */
+    public boolean isIndirectlyPoweringTo(World blocks, int x, int y, int z, int side)
+    {
+        TileEntity tile = blocks.getBlockTileEntity(x, y, z);
+        if (tile instanceof TileReceiver)
+        {
+            return ((TileReceiver)tile).isActive();
+        }
+        return false;
+    }
+
+    /**
+     * Is this block powering the block on the specified side
+     */
+    public boolean isPoweringTo(IBlockAccess blocks, int x, int y, int z, int side)
+    {
+        TileEntity tile = blocks.getBlockTileEntity(x, y, z);
+        if (tile instanceof TileReceiver)
+        {
+            return ((TileReceiver)tile).isActive();
+        }
+        return false;
+    }
+
+    public void onBlockPlaced(World world, int x, int y, int z, int side)
+    {
+        // Do this on client as well as server to avoid client-side display glitching
+        TileReceiver tile = (TileReceiver)world.getBlockTileEntity(x, y, z);
+        if (tile != null)
+        {
+            tile.orientation = side;
+        }
+    }
+
+    @Override
+    public boolean blockActivated(World world, int x, int y, int z, EntityPlayer player)
+    {
+        TileEntity tile = world.getBlockTileEntity(x, y, z);
+        if (tile instanceof TileReceiver)
+        {
+            // Check if the player is wielding a remote.
+            ItemStack item = player.getCurrentEquippedItem();
+            if (item != null && item.itemID == mod_InfraRedstone.IRRemoteItemID)
+            {
+                // Power On!
+                ((TileReceiver)tile).activate();
+                return false;
+            }
+            else // No remote
+            {
+                // Prevent GUI pop-up
+                if (player.isSneaking())
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- src/client/com/kaijin/InfraRedstoneRemote/TileReceiver.java
+++ src/client/com/kaijin/InfraRedstoneRemote/TileReceiver.java
@@ -1,12 +1,45 @@
 package com.kaijin.InfraRedstoneRemote;
 
-import com.kaijin.InfraRedstoneRemote.*;
-
 import net.minecraft.src.*;
 
 public class TileReceiver extends TileEntity
 {
-    private boolean previousPowerState = false;
+    private boolean powerOn = false;
+    private double interactDistSq = 64D;
+    private int powerDuration = 20;
+    private int powerTicks = 0;
+
+    public int orientation = 0;
+
+    public TileReceiver()
+    {
+        super();
+        setInteractionDistance(100D);
+        
+    }
+
+    public void setInteractionDistance(double dist)
+    {
+        interactDistSq = dist * dist;
+    }
+
+    public boolean isUseableByPlayer(EntityPlayer player)
+    {
+        if (worldObj.getBlockTileEntity(xCoord, yCoord, zCoord) != this)
+        {
+            return false;
+        }
+
+        // Check if the player is wielding a remote.
+        ItemStack item = player.getCurrentEquippedItem();
+        if (item != null && item.itemID == mod_InfraRedstone.IRRemoteItemID)
+        {
+            // Use a longer than normal distance test.
+            return player.getDistanceSq((double)xCoord + 0.5D, (double)yCoord + 0.5D, (double)zCoord + 0.5D) <= interactDistSq;
+        }
+        // Use a normal distance test.
+        return player.getDistanceSq((double)xCoord + 0.5D, (double)yCoord + 0.5D, (double)zCoord + 0.5D) <= 64D;
+    }
 
     @Override
     public boolean canUpdate()
@@ -14,20 +47,57 @@ public class TileReceiver extends TileEntity
         return true;
     }
 
-    public boolean isUseableByPlayer(EntityPlayer entityplayer)
+    @Override
+    public void updateEntity()
     {
-        if (worldObj.getBlockTileEntity(xCoord, yCoord, zCoord) != this)
+        if (powerTicks > 0)
         {
-            return false;
+            if (powerOn)
+            {
+                // Reduce timer
+                powerTicks--;
+                if (powerTicks == 0)
+                {
+                    // Turn off power and request block update
+                    powerOn = false;
+                    worldObj.markBlockNeedsUpdate(xCoord, yCoord, zCoord);
+                }
+            }
+            else
+            {
+                // Previously inactive; turn on and request a block update
+                powerOn = true;
+                worldObj.markBlockNeedsUpdate(xCoord, yCoord, zCoord);
+            }
         }
-
-        int itemID = 0;
-        // Check what item the player is wielding.
-        // Use a large distance test and return true only if a remote is found.
-        if (itemID == mod_InfraRedstone.IRRemoteItemID)
-            return entityplayer.getDistanceSq((double)xCoord + 0.5D, (double)yCoord + 0.5D, (double)zCoord + 0.5D) <= 128D;
-        else
-            return false;
     }
 
+    /*
+     *  Set or reset the timer (the rest will be handled in updateEntity)
+     */
+    public void activate()
+    {
+        powerTicks = powerDuration;
+    }
+
+    public boolean isActive()
+    {
+        return powerOn;
+    }
+
+    // Read tile entity from NBT.
+    public void readFromNBT(NBTTagCompound nbttagcompound)
+    {
+        super.readFromNBT(nbttagcompound);
+        powerTicks = nbttagcompound.getInteger("PowerTicks");
+        powerDuration = nbttagcompound.getInteger("PowerDuration");
+    }
+
+    // Write tile entity to NBT.
+    public void writeToNBT(NBTTagCompound nbttagcompound)
+    {
+        super.writeToNBT(nbttagcompound);
+        nbttagcompound.setInteger("PowerTicks", powerTicks);
+        nbttagcompound.setInteger("PowerDuration", powerDuration);
+    }
 }
